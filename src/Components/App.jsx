c2b64f16bc57fd183a625f8c7a2713b7cf778810
Mujtaba-Abbasi <gmabbsi92@gmail.com>
2022-09-21 03:11:33 -0700

    File and Folder structure with basic routes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,25 @@
-const App = () =>{
-    return <h1> App</h1>
-}
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Actors, Movies, Moviesinformation, Navbar, Profile } from "./index";
+import useStyles from './styles'
 
-export default App
+export default function App() {
+const classes = useStyles();
+
+  return (
+    <div className = {classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
+        <Routes>
+            <Route path="/" element={<Movies/>}/>
+            <Route exact path="/movies/:id" element={<Moviesinformation/>}/>
+            <Route exact path="/actors/:id" element={<Actors/>}/>
+            <Route exact path="/profile/:id" element={<Profile/>}/>
+        </Routes>
+      </main>
+    </div>
+  );
+}
---
+++ src/Components/Movies/Movies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Movies = () => {
+  return (
+    <div>
+    
+        Movies
+    
+    </div>
+  )
+}
+
+export default Movies
